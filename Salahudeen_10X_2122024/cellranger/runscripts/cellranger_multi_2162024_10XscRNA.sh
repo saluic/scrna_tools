0603f57d26831a05dd4986ac1e33eaadc4eb73da
trus-uic <142931687+trus-uic@users.noreply.github.com>
2024-02-19 19:30:06 -0600

    Update cellranger_multi_2162024_10XscRNA.sh

--- Salahudeen_10X_2122024/cellranger/runscripts/cellranger_multi_2162024_10XscRNA.sh
+++ Salahudeen_10X_2122024/cellranger/runscripts/cellranger_multi_2162024_10XscRNA.sh
@@ -5,7 +5,7 @@
 # You must define your BC barcodes as defined in the single-cell experimental preparation
 
 ./cellranger multi --id=Sample_S0117_2024 \
-        --csv=/projects/com_shared/ameen/lab/scrna_tools/repo/Salahudeen_10X_2122024/cellranger/2162024_cellranger_multi_params.csv \
+        --csv=/projects/com_shared/ameen/lab/scrna_tools/Salahudeen_10X_2122024/cellranger/2162024_cellranger_multi_params.csv \
         --output-dir /projects/com_shared/ameen/lab/data/scRNAseq/Salahudeen_10X_2024212/run_cellranger_count_2162024
 
 done
